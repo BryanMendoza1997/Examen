85c1e2af02fdbc02a6066a3412608ce6dd009e96
BryanMendoza1997 <bryan.mendoza2015@uteq.edu.ec>
2020-07-23 18:39:17 -0500

    Tercer Commit

--- app/src/main/java/com/example/examen/Principal.java
+++ app/src/main/java/com/example/examen/Principal.java
@@ -3,6 +3,17 @@ package com.example.examen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.examen.WebServices.Asynchtask;
+import com.example.examen.WebServices.WebService;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Principal extends AppCompatActivity {
 
@@ -10,5 +21,21 @@ public class Principal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        Map<String, String> datos = new HashMap<String, String>();
+        WebService ws= new WebService("https://restcountries.eu/rest/v2/all", datos, Principal.this, (Asynchtask) Principal.this);
+        ws.execute("");
+
+    }
+
+    public void processFinish(String result) throws JSONException {
+        String lstBancos="";
+        JSONArray JSONlista =  new JSONArray(result);
+        for(int i=0; i< JSONlista.length();i++){
+            JSONObject banco=  JSONlista.getJSONObject(i);
+            lstBancos = lstBancos + "\n" + banco.getString("name").toString();
+
+        }
+
+
     }
 }
